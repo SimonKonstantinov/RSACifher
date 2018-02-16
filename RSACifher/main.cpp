e892565577f1366ebbee39921301790545c0a726
Simon Konstantinov <Sim-PC@yandex.ru>
2018-02-16 16:14:27 +0300

    странный код,. Буду его исправлять

--- RSACifher/main.cpp
+++ RSACifher/main.cpp
@@ -2,17 +2,17 @@
 #include<math.h>
 #include<string.h>
 #include<stdlib.h>
-
+#include"Header.h"
 using namespace std;
 
-long int p, q, n, t, flag, e[100], d[100], j, temp[100], m[100], en[100], i;
+long int p, q, n, flag, e[100], d[100], t,j, temp[100], m[100], en[100];
 char msg[100];
-int prime(long int);
-void ce();
-long int cd(long int);
-void encrypt();
-void decrypt();
-int prime(long int pr)
+//int prime(long int);
+//void ce();
+//long int cd(long int);
+//void encrypt();
+//void decrypt();
+/*int prime(long int pr)
 {
 	int i;
 	j = sqrt(pr);
@@ -22,24 +22,24 @@ int prime(long int pr)
 			return 0;
 	}
 	return 1;
-}
+}*/
 int main()
 {
 	char ch = '0';
 	do {
 		cout << "\nENTER FIRST PRIME NUMBER\n";
 		cin >> p;
-		flag = prime(p);
+		flag = prime(p,j);
 		while (flag == 0)
 		{
 			cout << "\nWRONG INPUT\n";
 			cout << "\nENTER FIRST PRIME NUMBER\n";
 			cin >> p;
-			flag = prime(p);
+			flag = prime(p,j);
 		}
 		cout << "\nENTER SECOND PRIME NUMBER\n";
 		cin >> q;
-		flag = prime(q);
+		flag = prime(q,j);
 		while (flag == 0 || p == q)
 		{
 			if (p == q) {
@@ -53,102 +53,22 @@ int main()
 			cout << "\nENTER SECOND PRIME NUMBER" << endl;
 
 			cin >> q;
-			flag = prime(q);
+			flag = prime(q,j);
 
 		}
 		cout << "\nENTER MESSAGE" << endl;
 		fflush(stdin);
 		cin >> msg;
-		for (i = 0; msg[i] != '\0'; i++)
+		for (int i = 0; msg[i] != '\0'; i++)
 			m[i] = msg[i];
 		n = p * q;
 		t = (p - 1) * (q - 1);
-		ce();
+		ce(t,q,p);
 		cout << "\nPOSSIBLE VALUES OF e AND d ARE" << endl;
-		for (i = 0; i < j - 1; i++)
+		for (int i = 0; i < j - 1; i++)
 			cout << e[i] << "\t" << d[i] << "\n";
-		encrypt();
-		decrypt();
+		encrypt(m, temp, n, msg);
+		decrypt(m, temp);
 	} while (ch != 27);
 	return 0;
-}
-void ce()
-{
-	int k;
-	k = 0;
-	for (i = 2; i < t; i++)
-	{
-		if (t % i == 0)
-			continue;
-		flag = prime(i);
-		if (flag == 1 && i != p && i != q)
-		{
-			e[k] = i;
-			flag = cd(e[k]);
-			if (flag > 0)
-			{
-				d[k] = flag;
-				k++;
-			}
-			if (k == 99)
-				break;
-		}
-	}
-}
-long int cd(long int x)
-{
-	long int k = 1;
-	while (1)
-	{
-		k = k + t;
-		if (k % x == 0)
-			return (k / x);
-	}
-}
-void encrypt()
-{
-	long int pt, ct, key = e[0], k, len;
-	i = 0;
-	len = strlen(msg);
-	while (i != len)
-	{
-		pt = m[i];
-		pt = pt - 96;
-		k = 1;
-		for (j = 0; j < key; j++)
-		{
-			k = k * pt;
-			k = k % n;
-		}
-		temp[i] = k;
-		ct = k + 96;
-		en[i] = ct;
-		i++;
-	}
-	en[i] = -1;
-	cout << "\nTHE ENCRYPTED MESSAGE IS\n";
-	for (i = 0; en[i] != -1; i++)
-		printf("%c", en[i]);
-}
-void decrypt()
-{
-	long int pt, ct, key = d[0], k;
-	i = 0;
-	while (en[i] != -1)
-	{
-		ct = temp[i];
-		k = 1;
-		for (j = 0; j < key; j++)
-		{
-			k = k * ct;
-			k = k % n;
-		}
-		pt = k + 96;
-		m[i] = pt;
-		i++;
-	}
-	m[i] = -1;
-	cout << "\nTHE DECRYPTED MESSAGE IS\n";
-	for (i = 0; m[i] != -1; i++)
-		printf("%c", m[i]);
 }
